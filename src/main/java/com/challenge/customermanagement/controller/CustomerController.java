b041f9ef9a45db8088a5cd903cdf5f3d1f62a5f8
Mariano Godoy <50596171+marianogodoy82@users.noreply.github.com>
2025-02-15 19:54:35 -0300

    feat: statistics endpoint / jacoco implementation

--- src/main/java/com/challenge/customermanagement/controller/CustomerController.java
+++ src/main/java/com/challenge/customermanagement/controller/CustomerController.java
@@ -10,7 +10,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.challenge.customermanagement.dto.MetricsResponse;
 import com.challenge.customermanagement.entity.Customer;
+import com.challenge.customermanagement.model.CustomerMetrics;
+import com.challenge.customermanagement.service.CustomerMetricsService;
 import com.challenge.customermanagement.service.CustomerService;
 
 import lombok.RequiredArgsConstructor;
@@ -21,6 +24,7 @@ import lombok.RequiredArgsConstructor;
 public class CustomerController {
 
    private final CustomerService customerService;
+   private final CustomerMetricsService customerMetricsService;
 
    @PostMapping
    public Customer createCustomer(@RequestBody Customer customer) {
@@ -31,9 +35,18 @@ public class CustomerController {
    public List<Customer> retrieveCustomers() {
       return customerService.retrieveCustomers();
    }
-
    @GetMapping("/{id}")
    public Customer retrieveCustomerById(@PathVariable UUID id) {
       return customerService.retrieveCustomerById(id);
+   }
+
+   @GetMapping("/statistics")
+   public MetricsResponse getStatistics() {
+      final CustomerMetrics metrics = customerMetricsService.retrieveMetrics();
+      return MetricsResponse.builder()
+             .totalCustomer(metrics.totalCustomer())
+             .averageAge(metrics.averageAge())
+             .standardDeviation(metrics.standardDeviation())
+             .build();
    }
 }
